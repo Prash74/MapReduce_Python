95c6bf53f5ef86bfa38d211bc6b8f72e3e452f2c
Prashanth <prashanthbalasubramani@gmail.com>
2017-09-18 21:22:02 -0400

    Hadoop Streaming Fix

--- wordcount/mapper.py
+++ wordcount/mapper.py
@@ -4,9 +4,9 @@ import sys
 
 def mapper():
     for line in sys.stdin:
-        data = line.strip().split(' ')
-        for word in data:
-            print "{0}\t{1}".format(word, 1)
+        date = line.strip().split(',')[1]
+        year = date.split('-')[0]
+        print "{0}\t{1}".format(year, 1)
 
 if __name__ == "__main__":
     mapper()
